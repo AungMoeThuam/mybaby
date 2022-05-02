dc8b763cbd45ffed4449a75edadaa0b1d49a4494
aung moe thu <aungmoethu.2020.mc@gmail.com>
2022-05-02 19:53:21 +0630

    added files

---
+++ Hello.js
@@ -0,0 +1,36 @@
+var a;
+a = "Hello Earth";
+console.log(a + " Hello sun")
+
+var num1= 100;
+var num2=200;
+console.log(num1+num2)
+
+var num=100;
+var num1=50;
+console.log(num-num1)
+
+var num1=400; num2= 2;
+console.log(num1/num2)
+
+var a= 10; b= 20;
+console.log(a*b)
+
+function aa(num1,num1){
+  let result= num1% num2 ; console.log(result);
+} aa(100,7);
+
+function nn(height){
+  if (height>5){ console.log("u are tall");
+} else if (height<5){ console.log("u are short");
+}
+else{ console.log("u are young");}
+}
+nn(4);
+  
+  
+  
+  
+  
+
+
